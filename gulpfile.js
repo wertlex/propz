2a2678597b2239a5a9cc764c6bc1efc6fa72d758
Wert_Lex <a.v.povar@gmail.com>
2016-05-11 15:21:20 +0600

    linting tests

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@ const   gulp    = require('gulp'),
         eslint  = require('gulp-eslint'),
         mocha   = require('gulp-mocha');
 
-const SOURCE = './src';
+const   SOURCE      = './src',
+        TEST_SOURCE = './test';
 
 gulp.task('lint', function(){
     return gulp
         .src([SOURCE + '/*.js', SOURCE + '/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format());
+});
+
+gulp.task('lint-test', function(){
+    return gulp
+        .src([TEST_SOURCE + '/*.js', TEST_SOURCE + '/**/*.js'] )
         .pipe(eslint())
         .pipe(eslint.format());
 });
