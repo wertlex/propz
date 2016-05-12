e3be23b2f1e9cb560eac2594d9d0f4b9a5a2d322
Wert_Lex <a.v.povar@gmail.com>
2016-05-12 23:31:00 +0600

    trying to be ready for publishing

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const   gulp    = require('gulp'),
         eslint  = require('gulp-eslint'),
         mocha   = require('gulp-mocha');
 
-const   SOURCE      = './src',
-        TEST_SOURCE = './test';
+const   SOURCE              = './src',
+        TEST_SOURCE         = './test',
+        UNIT_TEST_SOURCE    = './test/unit';
 
 gulp.task('lint', function(){
     return gulp
@@ -18,4 +19,11 @@ gulp.task('lint-test', function(){
         .src([TEST_SOURCE + '/*.js', TEST_SOURCE + '/**/*.js'] )
         .pipe(eslint())
         .pipe(eslint.format());
+});
+
+/** run unit tests */
+gulp.task('test', ['lint-test'], function(){
+    return gulp
+        .src([UNIT_TEST_SOURCE + '/*.js', UNIT_TEST_SOURCE + '/**/*.js'])
+        .pipe(mocha({reporter: 'spec'}));
 });
