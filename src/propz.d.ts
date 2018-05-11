517633213594eb6af7f1c03bc8f54f0ab62f027b
Alex Povar <a.v.povar@gmail.com>
2018-05-12 00:37:22 +0600

    Typings are here

---
+++ src/propz.d.ts
@@ -0,0 +1,4 @@
+export function get(object: any, path: Array<string|number>, defaultValue?: any): any
+export function set(object: any, path: Array<string|number>, value: any): any
+export function getClosest(object: any, path: Array<string|number>): any
+export function getDeepKeys(object: any, prefix: any): any[]
